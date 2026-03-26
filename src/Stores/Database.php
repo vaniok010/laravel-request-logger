79bbc10ee203f415f4ea9ee53fe1231056c9786e
Max Harasym <harasym28@gmail.com>
2026-03-27 01:22:42 +0200

    fix: improve table name handling

--- src/Stores/Database.php
+++ src/Stores/Database.php
@@ -58,12 +58,12 @@ class Database implements Store
     public function create(LogData $logData): void
     {
         try {
-            $tableName = config('request-logger.table_name').'_fingerprints';
+            $fingerprintsTable = (new RequestLogFingerprint())->getTable();
 
             RequestLogFingerprint::query()->upsert(
                 ['fingerprint' => $logData->fingerprint, 'repeats' => 1],
                 ['fingerprint'],
-                ['repeats' => DB::raw("$tableName.repeats + 1")]
+                ['repeats' => DB::raw("$fingerprintsTable.repeats + 1")]
             );
 
             $fingerprint = RequestLogFingerprint::query()->where('fingerprint', $logData->fingerprint)->firstOrFail();
